d985ad2117680fdc3dffc6652d736023c915e703
langan.kg Langan <langan.kg@gmail.com>
2021-04-02 08:58:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
 
   holiday_hash[:winter].map do |key, value|
-    binding.pry
+  
     value
   end.flatten
   # return an array of all of the supplies that are used in the winter season
