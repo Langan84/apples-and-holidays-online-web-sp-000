9eb7a6490b076528c25396ac4e33509f9cc7f3b9
langan.kg Langan <langan.kg@gmail.com>
2021-04-02 09:53:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do (season, holiday)
       puts "#{season.to_s.capitalize}:"
       holiday.each do |event, supplies|
-      puts "#{event.to_s.split("_").map {|x| x.capitalize}.join(" ") }: #{supplies.join.(", ")}"
+      puts "#{event.to_s.split("_").map {|x| x.capitalize }.join(" ") }: #{supplies.join.(", ")}"
     end
     end
   end
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
