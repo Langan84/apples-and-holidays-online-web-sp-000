d6b0049da8f99970a8697732d191b4e848726342
langan.kg Langan <langan.kg@gmail.com>
2021-04-02 09:37:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,6 +56,9 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
+
+  holiday_hash.each do (season, holidays)
+  end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
