34ca0052ca47a658511171fb6f48e8843fdddc44
langan.kg Langan <langan.kg@gmail.com>
2021-04-02 09:53:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,9 +57,9 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
 
-    holiday_hash.each do (season, holidays)
+    holiday_hash.each do (season, holiday)
       puts "#{season.to_s.capitalize}:"
-      holidays.each do |event, supplies|
+      holiday.each do |event, supplies|
       puts "#{event.to_s.split("_").map {|x| x.capitalize}.join(" ") }: #{supplies.join.(", ")}"
     end
     end
