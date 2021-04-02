b8525293483798fec5e835f2883d1586b0dd8b66
langan.kg Langan <langan.kg@gmail.com>
2021-04-02 10:22:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,5 +81,5 @@ def all_holidays_with_bbq(holiday_hash)
   end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  end.flattern.compact
+end.flatten.compact
 end
